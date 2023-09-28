996bc2e3db97b3bff34565a2c9b3fc633842f482
J Onyeani <jon.onyeani@gmail.com>
2023-09-28 10:23:21 +0100

    Greve changed to weep.

--- sh_script.sh
+++ sh_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Joseph shall return to Canaan, grieve not."
-echo 'Hovels shall turn to rose gardens, grieve not.'
+echo 'Hovels shall turn to rose gardens, weep not.'
 echo 'There is no road without an end, grieve not.'
 ls -lh
 echo "Here we are"
